a2f33a6db744cc7962374c0b4e63e18e82f6414a
Ram Yalamanchilli <ram@MacBookPro.lan>
2026-05-28 23:25:02 -0500

    lol

---
+++ github_test.py
@@ -0,0 +1,21 @@
+john_freddy = 21
+print(john_freddy)
+
+apes = ['gorrila', 'orangutan', 'chimps']
+
+for x in range(len(apes)):
+    print(apes[2])
+
+print(apes[1])
+print(apes[0])
+
+apes.append('human')
+
+print(apes[3])
+
+#github testing for me 3 and you too 2 and me 1 nobody 0
+
+#remote [helps connect vs code to git hub] (git remote add origin (SSH))
+#add the files (git add (filename))
+#commit the files with a message (git commit -m ("message"))
+#push it to github (git push -upstream origin main)
